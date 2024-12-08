1bce4190c9ec26c0e0bf3452f6fd6e738b1d72e4
Colin Smith <colinsmith497@gmail.com>
2024-12-07 23:28:49 -0500

    Implemented fairness metrics

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import numpy as np
 from pathlib import Path
 from PIL import Image
+import torch
 import torch.utils.data as data
 import torchvision.transforms as transforms
 
@@ -36,3 +37,29 @@ class ImageDataset(data.Dataset):
 
     def __len__(self):
         return len(self.img_paths)
+
+
+class FeatureDataset(data.Dataset):
+    def __init__(self, img_paths, img_size, net, device, transform=None):
+        net = net.to(device)
+
+        self.minimum_feature = 100
+        self.maximum_feature = 0
+        self.features = []
+        for img_path in img_paths:
+            img = Image.open(str(img_path))
+            if transform:
+                img = transform(img)
+            img = img.view(1, 3, img_size, img_size)  # reshape image to match model dimensions (1 batch size)
+            img = img.to(device)
+
+            outputs = net(img)["avgpool"].cpu().detach().numpy().squeeze()
+            self.minimum_feature = min(self.minimum_feature, outputs.min())
+            self.maximum_feature = max(self.maximum_feature, outputs.min())
+            self.features.append(torch.Tensor(outputs))
+
+    def __getitem__(self, idx):
+        return ((self.features[idx] - self.minimum_feature)/(self.maximum_feature - self.minimum_feature)*256).to(torch.uint8)
+
+    def __len__(self):
+        return len(self.features)
